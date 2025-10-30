9189e2482b37e58433db02959668b5cc68a3055e
ejcaie <evancaie705@gmail.com>
2025-10-30 14:50:38 -0400

    Added models and timer

--- Assets/Scripts/RoundManager.cs
+++ Assets/Scripts/RoundManager.cs
@@ -1,24 +1,38 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoundManager : MonoBehaviour
 {
     public PlayerHealth player1Manager;
     public PlayerHealth player2Manager;
+    public Slider timer;
 
     void Start()
     {
-        
+        timer.value = 5f;
+        StartRound();
     }
 
     void StartRound()
     {
         //player1Manager.currentState
         //player2Manager.switch(currentState).idle;
+        RoundUpdate();
+    }
+
+    void RoundUpdate()
+    {
+        if (timer.value > 0)
+        {
+            timer.value = timer.value - 1 * Time.deltaTime;
+            RoundUpdate();
+        }
+        else EndRound();
     }
 
     public void EndRound()
     {
-
+        print("its so over");
     }
 
 }
